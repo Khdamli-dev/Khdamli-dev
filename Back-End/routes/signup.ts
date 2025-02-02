ac15796758fa1101d6e52b17373a12d7e5d2287b
Beloued Bouabdellah <bouaabd10@gmail.com>
2025-02-02 19:25:19 +0100

    move getRegions and getCities functions to address router, because they will used also when user want to update his address not just in signup

--- Back-End/routes/signup.ts
+++ Back-End/routes/signup.ts
@@ -1,12 +1,8 @@
 import express, { Router } from 'express';
 import checkInfo from '../middleware/checkInfo';
-import getRegions from '../utils/address/getRegions';
-import getCities from '../utils/address/getCities';
 
 const signup: Router = express.Router();
 
 signup.post('/credentials', checkInfo);   // we put function that create user after checkInfo
-signup.get('/regions', getRegions);
-signup.get('/cities', getCities);
 
 export default signup;
