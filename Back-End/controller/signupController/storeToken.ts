ba19622d4ff3310a3e5add4b460c1accab350744
Beloued Bouabdellah <bouaabd10@gmail.com>
2025-02-04 18:17:07 +0100

    make the route that resend confirmation email

--- Back-End/controller/signupController/storeToken.ts
+++ Back-End/controller/signupController/storeToken.ts
@@ -4,7 +4,9 @@ const storeToken = async (userId : number, token : string) => {
     await pool.query(`
         INSERT INTO confirmation_token(user_id, token)
         values ($1,$2)
+        ON CONFLICT (user_id) DO UPDATE
+        SET token = EXCLUDED.token
         `,[userId,token]);
-} 
+}
 
 export default storeToken;
