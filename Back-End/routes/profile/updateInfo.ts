b118473bd8dd15924b5f6636864a6a6214722158
Beloued Bouabdellah <bouaabd10@gmail.com>
2025-02-24 12:40:14 +0100

    add the function that update role of client to be worker

--- Back-End/routes/profile/updateInfo.ts
+++ Back-End/routes/profile/updateInfo.ts
@@ -3,11 +3,13 @@ import { sendOTP } from '../../controller/profile/handlePasswordReset';
 import { verifyOTP } from '../../middleware/verifyOtp';
 import updateProfile from '../../controller/profile/updateProfile';
 import assignAddress from '../../middleware/assignAddress';
+import role from './role';
 
 const update: Router = express.Router();
 
 update.post('/user-info', assignAddress, updateProfile);
 update.post('/password-reset/request', sendOTP); // Request OTP for password reset
 update.post('/password-reset/reset', verifyOTP, updateProfile);
+update.use('/role', role);
 
 export default update;
