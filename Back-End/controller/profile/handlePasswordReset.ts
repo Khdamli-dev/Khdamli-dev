9027727bc0babf5bd8b24c7eddb279687823ebf0
Mossab <mm.latrache@esi-sba.dz>
2025-03-04 12:17:51 +0100

    adjusted verifyOTP route and added middleware to check if otp can be sent

--- Back-End/controller/profile/handlePasswordReset.ts
+++ Back-End/controller/profile/handlePasswordReset.ts
@@ -6,7 +6,7 @@ import { passwordResetMail} from "../../utils/mailer/emailBody";
 
 export const sendOTP = async (req: Request, res: Response) => {
   try {
-    const { email } = req.body;
+    const { email } = req.body.credentials;
     if (!email) {
       res.status(400).json({ message: "Email is required" ,
         success : false,
@@ -34,5 +34,60 @@ export const sendOTP = async (req: Request, res: Response) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const verifyOTP = async (req: Request, res: Response) => {
+  try {
+    const { id, otp } = req.body;
+
+    if (!id || !otp) {
+     res.status(400).json({ 
+      message: "User ID and OTP are required" ,
+      success : false,
+      resend : false
+    });
+     return ;
+    }
+
+    // Verify OTP from otp_codes table
+    const { rows } = await pool.query(
+      `SELECT expires_at FROM otp_codes WHERE user_id = $1 AND otp = $2`,
+      [id, otp]
+    );
+
+    if (!rows.length) {
+      res.status(400).json({
+         message: "Invalid OTP" ,
+         success : false,
+         resend : false
+        });
+      return ;
+    }
+
+    if (new Date() > new Date(rows[0].expires_at)) {
+      res.status(400).json({
+         message: "OTP has expired" ,
+         success : false,
+         resend : true
+        });
+      return ;
+    }
+
+    await pool.query(`DELETE FROM otp_codes WHERE user_id = $1`, [id]);
+    res.status(200).json({ 
+      message: "OTP successfully verified" ,
+      success : true
+    });
+    return ;
+
+  } catch (error) {
+    console.error("Error verifying OTP:", error);
+    res.status(500).json({ 
+      message: "Internal server error" , 
+      success : false,
+      resend : false
+    });
+    return ;
   }
 };
---
+++ Back-End/middleware/canSendOTP.ts
@@ -0,0 +1,47 @@
+import { Request , Response , NextFunction } from "express";
+import Credentials from "../interface/credentials";
+import pool from "../database/dbConnection";
+export const canSendOTP = async (req : Request , res : Response , next : NextFunction) => {
+    try {
+  const {email} : Credentials = req.body.credentials;
+  if (!email) {
+    res.status(400).json({
+        message: "Email is required",
+        success : false,
+        resend : false
+       });
+       return;
+  }
+  const { rows } = await pool.query(`
+    SELECT oc.* FROM otp_codes oc
+JOIN "user" u ON u.id = oc.user_id
+WHERE u.email =$1`,[email]
+  );
+  if (!rows.length) {
+     next();
+     return;
+    }
+  if (new Date() <= new Date(rows[0].expires_at)) {
+    res.status(429).json({
+       message: "OTP resend not allowed. Please wait until the previous OTP expires." ,
+       success : false,
+       resend : false
+      });
+    return ;
+  };
+  await pool.query(
+    `DELETE FROM otp_codes WHERE user_id = (SELECT id FROM "user" WHERE email = $1)`,
+    [email]
+  );
+  next();
+  return;
+
+} catch (err) {
+  res.status(500).json({
+    message : "internal server error" ,
+    success : false,
+    resend : false
+  })
+  return;
+}
+};
