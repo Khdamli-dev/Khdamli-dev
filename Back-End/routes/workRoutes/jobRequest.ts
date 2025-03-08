f820857abeaf4289c7c8f9519f312c53edf7502f
Beloued Bouabdellah <bouaabd10@gmail.com>
2025-03-08 16:05:20 +0100

    make the function that get public requests for specific client

--- Back-End/routes/workRoutes/jobRequest.ts
+++ Back-End/routes/workRoutes/jobRequest.ts
@@ -2,12 +2,15 @@ import express, { Router } from 'express';
 import createRequest from '../../controller/jobRequestController/createRequest';
 import validateJobRequest from '../../middleware/validateJobRequest';
 import getPrivateRequest from '../../controller/jobRequestController/getPrivateRequest';
+import getPublicRequest from '../../controller/jobRequestController/getPublicRequest';
 
 const request:Router = express.Router();
 
 request.post('/create', validateJobRequest, createRequest);
-
 // this route used to get private requests for a client or a worker
 request.get('/get/private', getPrivateRequest);
+// this route used to get public requests of client
+request.get('/get/public/:client', getPublicRequest);
+
 
 export default request;
