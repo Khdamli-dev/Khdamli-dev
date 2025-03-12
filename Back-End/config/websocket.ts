8ee281a2d5bf0aeea0302266cc87acc997501850
Beloued Bouabdellah <bouaabd10@gmail.com>
2025-03-12 14:30:49 +0100

    add websocket connection

---
+++ Back-End/config/websocket.ts
@@ -0,0 +1,29 @@
+import { Server, Socket } from 'socket.io';
+import http from 'http';
+
+let io : Server;
+
+export const initializeWebSocket = (server : http.Server): Server => {
+    io = new Server(server, {
+        cors : {
+            origin : ["http://localhost:8081", "http://127.0.0.1:5500", "http://localhost:5500"], // origins that allowed to make requests
+            methods : ["GET", "POST"]
+        }
+    });
+
+    io.on('connection', (socket : Socket) => {
+        console.log(`client connected to websocket with id : ${socket.id}`);
+        socket.on('disconnect', () => {
+            console.log(`Client disconnected: ${socket.id}`);
+        });
+    });
+
+    return io;
+}
+
+export const getIo = () : Server => {
+    if (!io){
+        throw new Error("websocket not initialized");
+    }
+    return io;  
+}
--- Back-End/server.ts
+++ Back-End/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import http from 'http';
 import mainRouter from './routes/mainRoutes/mainRoute';
+import { initializeWebSocket } from './config/websocket';
 
 dotenv.config();
+
 const app = express();
+const server = http.createServer(app); // http server
 const port = process.env.PORT;
 
 // allow transfer json data to requests
@@ -12,6 +16,9 @@ app.use(express.json());
 // make the main route
 app.use('/', mainRouter);
 
-app.listen(port, () => {
+// initialize websocket
+initializeWebSocket(server);
+
+server.listen(port, () => {
   console.log(`app listen in port ${port}`);
 });
