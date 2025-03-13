4eef1d063509231f943163f118f358b8037e2c9b
Mossab <mm.latrache@esi-sba.dz>
2025-03-13 16:55:00 +0100

    no change

--- Back-End/controller/jobRequestController/deleteRequest.ts
+++ Back-End/controller/jobRequestController/deleteRequest.ts
@@ -3,14 +3,15 @@ import { Request , Response } from "express";
 
 const deleteRequest = async (req : Request , res : Response) => {
     try {
-        const requestId : number= +req.params.requestId;
-        if (Number.isNaN(requestId)){
+        
+        if (Number.isNaN(req.params.requestId)){
           res.status(400).json({
           message: 'Please provide request id',
           success : false
           });
           return;
           }
+          const requestId : number= +req.params.requestId;
         await pool.query(`DELETE FROM request WHERE id=$1`,[requestId]);
         res.status(200).json({ 
             message : "request deleted successfully",
