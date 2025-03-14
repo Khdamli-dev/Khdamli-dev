3d467e22751ceccacd19d37cf67a613ddbfa8c25
Beloued Bouabdellah <bouaabd10@gmail.com>
2025-03-14 07:57:53 +0100

    add cors to the app

--- Back-End/server.ts
+++ Back-End/server.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import http from 'http';
+import cors from "cors";
 import mainRouter from './routes/mainRoutes/mainRoute';
 import { initializeWebSocket } from './config/websocket';
+import corsOption from './config/cors';
 
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app); // http server
 const port = process.env.PORT;
+
+// allow cors middleware
+app.use(cors(corsOption));
 
 // allow transfer json data to requests
 app.use(express.json());
