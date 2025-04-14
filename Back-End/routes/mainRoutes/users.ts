6fa36ab1c450de4bf6add63aa0628e5ab4f60a34
Mino <mino@kali>
2025-04-14 12:11:51 +0100

    feat: Add worker profile info fetching endpoint

--- Back-End/routes/mainRoutes/users.ts
+++ Back-End/routes/mainRoutes/users.ts
@@ -5,6 +5,7 @@ import changeToWorkerRole from "../../controller/profile/changeToWorkerRole";
 import { uploadProfilePicture } from "../../controller/upload/uploadProfilePicture";
 import deleteUser from '../../controller/profile/deleteUser';
 import getClientProfile from '../../controller/profile/getClientProfile'; 
+import getWorkerProfile from '../../controller/profile/getWorkerProfile';
 
 const users: Router = express.Router();
 
@@ -13,4 +14,5 @@ users.put('/:id/role/worker', changeToWorkerRole);
 users.put('/:id/profile-picture',uploadProfilePicture);
 users.delete('/:id', deleteUser);
 users.get('/client/:id', getClientProfile);
+users.get('/worker/:id', getWorkerProfile);
 export default users;
