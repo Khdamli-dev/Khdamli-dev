a6ab51635feb93ca8205e583add514d5355968d5
Beloued Bouabdellah <bouaabd10@gmail.com>
2025-04-30 19:32:00 +0100

    make the function of refreshAccessToken return all the info of user not just userId and role

--- Back-End/config/refreshAccessToken.ts
+++ Back-End/config/refreshAccessToken.ts
@@ -3,12 +3,13 @@ import jwt from 'jsonwebtoken';
 import { Request, Response } from 'express';
 import makeJwtToken from '../utils/authentication/makeJwtToken';
 import { JwtToken, JwtUserPayload } from '../interface/jwtToken';
+import pool from '../database/dbConnection';
 
 dotenv.config();
 
 const refreshAccessToken = (req: Request, res: Response) => {
-
-  const refreshTokenSecret: string | undefined = process.env.Refresh_Token_Secret;
+  const refreshTokenSecret: string | undefined =
+    process.env.Refresh_Token_Secret;
   if (!refreshTokenSecret) {
     res.status(500).json({
       message: 'internal error',
@@ -18,7 +19,8 @@ const refreshAccessToken = (req: Request, res: Response) => {
   }
 
   const refreshHeader = req.headers['x-refresh-token'] as string;
-  const refreshToken: string = req.cookies?.refreshToken || refreshHeader?.split(' ')[1];
+  const refreshToken: string =
+    req.cookies?.refreshToken || refreshHeader?.split(' ')[1];
   if (!refreshToken) {
     res.status(403).json({
       message: 'you are forbidden, dont have refresh token',
@@ -28,7 +30,7 @@ const refreshAccessToken = (req: Request, res: Response) => {
   }
 
   // check refresh token
-  jwt.verify(refreshToken, refreshTokenSecret, (err, decode) => {
+  jwt.verify(refreshToken, refreshTokenSecret, async (err, decode) => {
     if (err) {
       res.status(403).json({
         message: 'you are forbidden, fake refresh token',
@@ -39,21 +41,36 @@ const refreshAccessToken = (req: Request, res: Response) => {
 
     // generate new access token
     const decodedToken: JwtUserPayload = decode as JwtUserPayload;
-    const { userId, role }: { userId: string; role: string } =
-      decodedToken.userInfo;
+    const { userId }: { userId: string } = decodedToken.userInfo;
+
+    // get user
+    const { rows: user } = await pool.query(
+      `
+        SELECT * from "user"
+        WHERE id=$1
+        `,
+      [userId]
+    );
+    if (!user.length){
+      res.status(403).json({
+        message: "you are forbidden, don't have account",
+        success: false,
+      });
+      return;
+    }
+      
     let info: JwtToken = {
       userId,
-      role,
+      role : String(user[0].role),
       time: '30m',
       secret: process.env.Access_Token_Secret || '',
     };
     const accessToken = makeJwtToken(info);
     res.status(200).json({
-        success: true,
-        message: "generate new access token with success",
-        accessToken,
-        userId,
-        role
+      success: true,
+      message: 'generate new access token with success',
+      accessToken,
+      user : user[0]
     });
   });
 };
