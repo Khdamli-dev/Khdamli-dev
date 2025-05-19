5731f1cf0c2d4b96b5492c8de36d856ef59f2019
alaamohammedseghir <alaamohammedseghir@gmail.com>
2025-05-19 18:44:30 +1100

    some changes

--- Front-End/app/(tabs)/(profile)/index.tsx
+++ Front-End/app/(tabs)/(profile)/index.tsx
@@ -128,7 +128,6 @@ const ProfileScreen: React.FC<ProfileScreenProps> = ({ navigation }) => {
         const { id, role } = user;
         const endpoint =
           role === 1 ? `/users/client/` : role === 2 ? `/users/worker/` : null;
-        console.log(endpoint);
         if (endpoint) {
           const response = await apiClient.get(`${endpoint}${id}`);
           console.log(response);
