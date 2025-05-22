b7af303a8277a917a17dc91eb3b0a275682affb3
Mossab <mm.latrache@esi-sba.dz>
2025-05-22 14:57:01 +0100

    fixed type as any problem

--- Back-End/controller/profile/updateProfile.ts
+++ Back-End/controller/profile/updateProfile.ts
@@ -7,11 +7,12 @@ import validateInfo from "../../middleware/validateInfo";
 import updateWorkerInfo from "./updateWorkerProfile";
 import checkRole from "../../middleware/checkRole";
 import dotenv from "dotenv";
+import { workerInfo } from "../../interface/workerInfo";
 dotenv.config();
 const workerRoleId = Number(process.env.WORKER_ROLE_ID);
 const updateProfile = async (req: Request, res: Response) => {
     const id : number = +req.params.id;
-    const {personalInfo, credentials , workerInfo} : { personalInfo : PersonalInfo, credentials : Credentials , workerInfo : any} = req.body;
+    const {personalInfo, credentials , workerInfo} : { personalInfo : PersonalInfo, credentials : Credentials , workerInfo : workerInfo} = req.body;
 
     if (Number.isNaN(id)) {
        res.status(400).json({ message: 'user id is required' });
---
+++ Back-End/interface/workerInfo.ts
@@ -0,0 +1,4 @@
+export interface workerInfo{
+    categories:number[],
+    workingHours : { day: number, begin: string, end: string }[],
+}
