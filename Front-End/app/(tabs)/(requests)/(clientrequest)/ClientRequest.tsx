b0d5d8ae0770f3ab0a3a655573fb41c52d83f15c
KHALIL-hub5 <khalildjaidja510@gmail.com>
2025-05-24 12:33:23 +0100

    edit on the workers comments

--- Front-End/app/(tabs)/(requests)/(clientrequest)/ClientRequest.tsx
+++ Front-End/app/(tabs)/(requests)/(clientrequest)/ClientRequest.tsx
@@ -789,7 +789,6 @@ const PublicRequest = () => {
     const [commentText, setCommentText] = useState(initialComment || "");
     const commentInputRef = useRef(null);
     const [keyboardVisible, setKeyboardVisible] = useState(false);
-
     useEffect(() => {
       const keyboardDidShowListener = Keyboard.addListener(
         "keyboardDidShow",
